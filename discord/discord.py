b20454c9ecd09c80636e0e915a8767bbe38d9104
Mireu - <58178547+Mir-Labcon@users.noreply.github.com>
2020-10-07 01:08:42 +0900

    도커 파일 + 파이썬 + 셀 기본 세팅

---
+++ discord/discord.py
@@ -0,0 +1,17 @@
+import discord
+import asyncio
+import random
+from discord import Embed
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+bot = discord.Client()
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------')    
+
+bot.run("통큰")
