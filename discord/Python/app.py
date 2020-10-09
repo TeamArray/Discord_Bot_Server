6ffb051a80780a3f6434e2be96f7bbf468641308
Mireu - <58178547+Mir-Labcon@users.noreply.github.com>
2020-10-09 12:23:28 +0900

    오류 수정

---
+++ discord/Python/app.py
@@ -0,0 +1,21 @@
+import discord
+from discord import Embed
+from discord.ext import commands
+from discord.ext.commands import Bot
+
+bot = discord.Client()
+
+@bot.event
+async def on_ready():
+    print('Logged in as')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('------') 
+'''
+@bot.command()
+async def ping(ctx):
+    latancy = bot.latency
+    await ctx.send(f'Ping! {round(latancy*1000)}ms')
+'''
+
+bot.run("통큰")
